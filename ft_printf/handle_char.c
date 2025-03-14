bf496d6b4a9f22b9a38fb9de1e4ff1cf4b40d791
anzahoro <109831034+AnastasiaZahorodnia@users.noreply.github.com>
2025-03-14 22:33:52 +0100

    Delete ft_printf directory

--- ft_printf/handle_char.c
+++
@@ -1,8 +0,0 @@
-#include "ft_printf.h"
-
-int handle_char(va_list args)
-{
-    char c = va_arg(args, int);
-    write(1, &c, 1);
-    return 1;
-}
--- ft_printf/handle_percent.c
+++
@@ -1,7 +0,0 @@
-#include "ft_printf.h"
-
-int handle_percent(void)
-{
-    write(1, "%", 1);
-    return 1;
-}
